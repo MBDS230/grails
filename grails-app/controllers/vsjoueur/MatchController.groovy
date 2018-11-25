03c19bbe999e7f7c9491a2d6492047386be76559
Kevin <jonathan.jkevin@gmail.com>
2018-11-25 18:45:58 +0400

    Match Controller

--- grails-app/controllers/vsjoueur/MatchController.groovy
+++ grails-app/controllers/vsjoueur/MatchController.groovy
@@ -1,31 +1,72 @@
 package vsjoueur
 
+import DAO.JoueurDao
+import grails.converters.JSON
 import mapping.Demandematch
 import mapping.Joueur
+import utilitaire.ReturnObject
+import utilitaire.StatusHttp
 
 class MatchController {
 
     def demandeMatch()
     {
-        Joueur joueurSession = null; // GET SESSION joueur
-        if(joueurSession != null)
+        try
         {
-            int idAutreJoueur = Integer.parseInt(params.getProperty("idAutreJoueur"));
-            int duree = Integer.parseInt(params.getProperty("duree"));
-            int joueurConnecte = joueurSession.getIdjoueur();
-            new MatchService().demandeMatch(joueurConnecte, idAutreJoueur, duree);
+            Joueur joueurSession =(Joueur) session.getAttribute("SESSION_JOUEUR");
+            if(joueurSession != null)
+            {
+                int idAutreJoueur = Integer.parseInt(params.getProperty("idAutreJoueur"));
+                int duree = Integer.parseInt(params.getProperty("duree"));
+                int joueurConnecte = joueurSession.getIdjoueur();
+                new MatchService().demandeMatch(joueurConnecte, idAutreJoueur, duree);
+                StatusHttp statu = new StatusHttp(200, null, null);
+                ReturnObject ret = new ReturnObject();
+                ret.setStatus(statu)
+                render ret as JSON
+            }
+            else
+            {
+                redirect(controller: "game", action: "login")
+            }
+        } catch (Exception exc ) {
+            StatusHttp statu = new StatusHttp(500, exc.getMessage(), "/game/login");
+            ReturnObject ret = new ReturnObject();
+            ret.setStatus(statu)
+            render ret as JSON
         }
+        StatusHttp statu = new StatusHttp(500, null, "/game/login");
+        ReturnObject ret = new ReturnObject();
+        ret.setStatus(statu)
+        render ret as JSON
+
     }
 
     def listeDemandeRencontre()
     {
-        ArrayList<Demandematch> listDem = null;
-        Joueur joueurSession = null; // GET SESSION joueur
-        if(joueurSession != null)
+        try
         {
-            int idJoueurConnecte = joueurSession.getIdjoueur();
-            listDem = new MatchService().listeDemandeRencontre(idJoueurConnecte);
+            ArrayList<Demandematch> listDem = null;
+            Joueur joueurSession = (Joueur) session.getAttribute("SESSION_JOUEUR");
+            if(joueurSession != null)
+            {
+                int idJoueurConnecte = joueurSession.getIdjoueur();
+                listDem = new MatchService().listeDemandeRencontre(idJoueurConnecte);
+                StatusHttp statu = new StatusHttp(200, null, null);
+                ReturnObject ret = new ReturnObject();
+                ret.setStatus(statu)
+                ret.setObjet(listDem);
+                render ret as JSON
+            }
+        }catch (Exception exc ) {
+            StatusHttp statu = new StatusHttp(500, exc.getMessage(), "/game/login");
+            ReturnObject ret = new ReturnObject();
+            ret.setStatus(statu)
+            render ret as JSON
         }
-        return listDem;
+        StatusHttp statu = new StatusHttp(500, null, "/game/login");
+        ReturnObject ret = new ReturnObject();
+        ret.setStatus(statu)
+        render ret as JSON
     }
 }
